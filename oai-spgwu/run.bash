a86293e3c11fb0561904162ed59e3f89ab01ba54
sumichaaan <yoshifumi.sumida@okinawaopenlabs.org>
2020-04-03 16:34:36 +0900

    Modified binding port in run script

--- oai-spgwu/run.bash
+++ oai-spgwu/run.bash
@@ -9,7 +9,7 @@ docker run \
         -e PGWC_SX_IP_ADDRESS="192.168.56.13" \
 	-e PGWU_SGI_GW="192.168.56.1" \
         -p 8805:8805 \
-	-p 2123:2123 \
+	-p 2152:2152 \
         -h spgwu.openair4G.eur \
 	--net=oai_network \
 	--ip=192.168.56.14 \
