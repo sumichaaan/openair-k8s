227541e523b20e90d1660388dc97baefe8788cd0
sumichaaan <yoshifumi.sumida@okinawaopenlabs.org>
2020-04-03 15:43:48 +0900

    Added test run script

--- oai-enb/run-ubt18.bash
+++ oai-enb/run-ubt18.bash
@@ -1,9 +1,10 @@
 docker run -it \
         --privileged \
+        -ti \
         -d \
         -v $(pwd)/../configs:/config \
         -v /dev:/dev \
         -v /lib/modules:/lib/modules:ro \
         --net=oai_network \
-      --ip=192.168.56.100 \
-        oai-enb-20200325b1
+        --ip=192.168.56.100 \
+        oai-enb-20200403r1:ubt18
